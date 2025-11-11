4bc4beb9f42d749028e43da902673e3a1f7409ac
abdullahshafaqat <abdullahshafaqat14.com>
2025-11-11 13:57:08 +0500

    now fully fixed

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/dist");
   app.use(express.static(frontendPath));
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
