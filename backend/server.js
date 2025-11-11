650ba4e94e98de3fbb88dec5e0f8ebc7ad0038b3
abdullahshafaqat <abdullahshafaqat14.com>
2025-11-11 13:50:26 +0500

    fix the issue of deployment

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 import dotenv from "dotenv";
 import path from "path";
 import express from "express";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
 import { connectDB } from "./src/utils/db.js";
 import app from "./src/app.js";
 import { server } from "./src/utils/socket.js";
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 connectDB();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
-  );
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
